b2aaf48c17d0a10901561e6b9d4dc71485ceabf3
edvardofficial <102943223+edvardofficial@users.noreply.github.com>
2023-11-30 22:53:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 function submitForm() {
     var password = document.querySelector('.password').value;
     if (password == 'night night') {
-      alert("Night night ❤️❤️")
+      alert("Hey tu! Da tu, am o rugăminte să îmbraci căștile ❤️❤️")
+      alert("Okey, mersi ❤️❤️")
       window.location.href = "./page/page.html";
     } else {
       alert('Parola nu este corecta');
